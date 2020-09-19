bd69c5b99824907a528ddae15db0d735f7b42853
DeFiGuy <governancesale@protonmail.com>
2020-09-19 10:24:09 -0400

    Updates

---
+++ precache-manifest.54441ae1defe430c7a4acb937ad67f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac85b6fd364ecd6050eae512a03f429",
+    "url": "/staking.dxsale.network/index.html"
+  },
+  {
+    "revision": "e2cbfe928a159598f5ca",
+    "url": "/staking.dxsale.network/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3b7f15b29b00c1dd2532",
+    "url": "/staking.dxsale.network/static/js/2.a4872725.chunk.js"
+  },
+  {
+    "revision": "38f7d42aae3c97e77c304aa9465f32f8",
+    "url": "/staking.dxsale.network/static/js/2.a4872725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2cbfe928a159598f5ca",
+    "url": "/staking.dxsale.network/static/js/main.177c4f96.chunk.js"
+  },
+  {
+    "revision": "495f2a7d26328234bae1",
+    "url": "/staking.dxsale.network/static/js/runtime-main.195fe8d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffa77c5466598ae80421b4d7712954ae.js"
+  "/staking.dxsale.network/precache-manifest.54441ae1defe430c7a4acb937ad67f38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/staking.dxsale.network/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
