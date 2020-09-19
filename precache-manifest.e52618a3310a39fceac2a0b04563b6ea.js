1ccafa415167c3035c7db93d588018f762aa27c6
DeFiGuy <governancesale@protonmail.com>
2020-09-19 00:32:18 -0400

    Updates

---
+++ precache-manifest.e52618a3310a39fceac2a0b04563b6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97457a840da98127dcc4f5170fc29df1",
+    "url": "/Staking/index.html"
+  },
+  {
+    "revision": "624a9d7202b5d10bea37",
+    "url": "/Staking/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "913d8ebc8f7bd1dd159f",
+    "url": "/Staking/static/js/2.3494a940.chunk.js"
+  },
+  {
+    "revision": "38f7d42aae3c97e77c304aa9465f32f8",
+    "url": "/Staking/static/js/2.3494a940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624a9d7202b5d10bea37",
+    "url": "/Staking/static/js/main.43225c7c.chunk.js"
+  },
+  {
+    "revision": "9959ae0444314669d0df",
+    "url": "/Staking/static/js/runtime-main.5607139c.js"
+  }
+]);
